45b732fac019a675fc65fa6246a155e1896fa258
ElennHarutyunyan <harutyunyanelen421@gmail.com>
2026-03-07 12:46:00 +0400

    Initial commit: kursayin_oop interpreter code

---
+++ Token.h
@@ -0,0 +1,28 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include <string>
+
+enum class TokenType {
+    Number,
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    Assign,
+    Identifier,
+    Semicolon,
+    LParen,
+    RParen,
+    EndOfFile,
+    Print,
+};
+
+struct Token {
+    TokenType type;
+    std::string value;
+
+    Token(TokenType t, std::string v = "") : type(t), value(v) {}
+};
+
+#endif // TOKEN_H
